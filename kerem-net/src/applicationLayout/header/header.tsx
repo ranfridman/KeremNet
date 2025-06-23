80c8c4c1bad00432abd28664f2cd0dbae50bfb9f
DESKTOP-TEN4NAB\user <ran.fridman.06@gmail.com>
2025-06-23 18:09:03 +0300

    Added the logo to the header

--- kerem-net/src/applicationLayout/header/header.tsx
+++ kerem-net/src/applicationLayout/header/header.tsx
@@ -1,8 +1,12 @@
 import { Box } from "@mui/material";
 import "./header.css";
+import logo from '../../logo192.png';
+
 const header = ({ children }: { children?: React.ReactNode }) => {
     return (
         <Box className="header">
+            <img src={logo} alt="logo image" className="logo" />
+            {children}
         </Box>
     )
 }
