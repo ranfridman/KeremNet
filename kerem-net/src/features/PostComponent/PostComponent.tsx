9493d96402d5781283b1dfbf19dfb0c71485090c
DESKTOP-TEN4NAB\user <ran.fridman.06@gmail.com>
2025-06-23 15:50:35 +0300

    comments are showen only if needed

--- kerem-net/src/features/PostComponent/PostComponent.tsx
+++ kerem-net/src/features/PostComponent/PostComponent.tsx
@@ -12,19 +12,19 @@ const PostComponent:React.FC<PostComponentProps> = ({text, comments, likes, crea
   return (
     <div className="post">
       <div className="post-content">
-        <div className="post-text">{text}</div>
+        <p className="post-text">{text}</p>
       </div>
       <div className="post-info">
         <div className="post-likes">Likes: {likes}</div>
         <div className="post-creator">Creator: {creatorName}</div>
         <div className="post-date">Date: {date}</div>
       </div>
-      <div className="post-comments">
-        <div className="post-comments-title">Comments</div>
+       {comments.length > 0 && <div className="post-comments">
+       <div className="post-comments-title">Comments</div>
         {comments.map((comment, index) => (
           <div className="comment" key={index}>{comment}</div>
         ))}
-      </div>
+      </div>}
     </div>
   );
 };
