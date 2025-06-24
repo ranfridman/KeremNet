146440dc54221c11a5feb37fda8cc7ddc9342c93
DESKTOP-TEN4NAB\user <ran.fridman.06@gmail.com>
2025-06-24 14:56:11 +0300

    Added back the data from json file

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -3,39 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import HomePage, { HomePageProps } from "./features/HomePage/HomePage";
 import Data from "./Data/posts.json";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { AppProvider, type Navigation } from "@toolpad/core/AppProvider";
-import AppsIcon from '@mui/icons-material/Apps';
+import AppsIcon from "@mui/icons-material/Apps";
 function App() {
-  const posts: HomePageProps["initialPosts"] = [
-    {
-      text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quos.",
-      comments: {
-        comments: [
-          { commentContent: "test1", userName: "SDF", date: "test" },
-          { commentContent: "test1", userName: "test", date: "test" },
-          { commentContent: "test1", userName: "ASGD", date: "test" },
-          { commentContent: "test1", userName: "Rtest", date: "test" },
-        ],
-      },
-      likes: 1323,
-      creatorName: "Ran",
-      date: "3/3/3",
-    },
-    {
-      text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quos.",
-      comments: {
-        comments: [
-          { commentContent: "test1", userName: "SDF", date: "3/3/3" },
-          { commentContent: "test1", userName: "Rtest", date: "3/3/3" },
-        ],
-      },
-      likes: 1323,
-      creatorName: "test",
-      date: "test",
-    },
-  ];
+  const posts: HomePageProps["initialPosts"] = Data;
   const NAVIGATION: Navigation = [
     {
       kind: "header",
@@ -48,15 +21,15 @@ function App() {
     },
   ];
   const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#22817E',
+    palette: {
+      primary: {
+        main: "#22817E",
+      },
+      secondary: {
+        main: "#22817E",
+      },
     },
-    secondary: {
-      main: '#22817E',
-    },
-  },
-});
+  });
 
   return (
     <div className="App">
@@ -64,7 +37,13 @@ function App() {
         navigation={NAVIGATION}
         branding={{
           title: "KeremNet",
-          logo:<img src={logo} alt="logo image" style={{width: "7vh", height: "7vh", margin: "0.5vh"}}/> ,
+          logo: (
+            <img
+              src={logo}
+              alt="logo image"
+              style={{ width: "7vh", height: "7vh", margin: "0.5vh" }}
+            />
+          ),
         }}
         theme={theme}
       >
