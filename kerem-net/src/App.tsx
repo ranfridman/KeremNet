18cc9576bd301a7448dcf36f4e713cd3ce8ace44
DESKTOP-TEN4NAB\user <ran.fridman.06@gmail.com>
2025-06-24 14:36:53 +0300

    Redone all the app layout

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,26 +1,77 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HomePage from "./features/HomePage/HomePage";
+import HomePage, { HomePageProps } from "./features/HomePage/HomePage";
 import Data from "./Data/posts.json";
-import NavBar from "./applicationLayout/navbar/navbar";
-import Page from "./applicationLayout/page/page";
-import Header from "./applicationLayout/header/header";
-import MainSection from "./applicationLayout/mainSection/mainSection";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { AppProvider, type Navigation } from "@toolpad/core/AppProvider";
+import AppsIcon from '@mui/icons-material/Apps';
 function App() {
-  const posts = Data;
+  const posts: HomePageProps["initialPosts"] = [
+    {
+      text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quos.",
+      comments: {
+        comments: [
+          { commentContent: "test1", userName: "SDF", date: "test" },
+          { commentContent: "test1", userName: "test", date: "test" },
+          { commentContent: "test1", userName: "ASGD", date: "test" },
+          { commentContent: "test1", userName: "Rtest", date: "test" },
+        ],
+      },
+      likes: 1323,
+      creatorName: "Ran",
+      date: "3/3/3",
+    },
+    {
+      text: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quos.",
+      comments: {
+        comments: [
+          { commentContent: "test1", userName: "SDF", date: "3/3/3" },
+          { commentContent: "test1", userName: "Rtest", date: "3/3/3" },
+        ],
+      },
+      likes: 1323,
+      creatorName: "test",
+      date: "test",
+    },
+  ];
+  const NAVIGATION: Navigation = [
+    {
+      kind: "header",
+      title: "Navigation Bar",
+    },
+    {
+      segment: "page-2",
+      title: "Posts",
+      icon: <AppsIcon />,
+    },
+  ];
+  const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#22817E',
+    },
+    secondary: {
+      main: '#22817E',
+    },
+  },
+});
+
   return (
     <div className="App">
-      <Header>
-      </Header>
-      <MainSection>
-      <NavBar>
-      </NavBar>
-        
-      <Page>
-        <HomePage initialPosts={posts} />
-      </Page>
-      </MainSection>
+      <AppProvider
+        navigation={NAVIGATION}
+        branding={{
+          title: "KeremNet",
+          logo:<img src={logo} alt="logo image" style={{width: "7vh", height: "7vh", margin: "0.5vh"}}/> ,
+        }}
+        theme={theme}
+      >
+        <DashboardLayout>
+          <HomePage initialPosts={posts} />
+        </DashboardLayout>
+      </AppProvider>
     </div>
   );
 }
--- kerem-net/src/features/PostComponent/PostComponent.tsx
+++ kerem-net/src/features/PostComponent/PostComponent.tsx
@@ -1,36 +1,78 @@
-import './PostComponent.css';
-import { CardContent,Card } from '@mui/material';
-
-interface PostComponentProps {
-    text: string;
-    comments: string[];
-    likes: number;
-    creatorName: string;
-    date: string;
+import { useState } from "react";
+import "./PostComponent.css";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import {
+  ListItemText,
+  Avatar,
+  Stack,
+  ListItem,
+  CardContent,
+  Card,
+  ListItemIcon,
+  Typography,
+  List,
+} from "@mui/material";
+import CommentSection, { CommentSectionProps } from "./CommentSection";
+export interface PostComponentProps {
+  text: string;
+  comments: CommentSectionProps;
+  likes: number;
+  creatorName: string;
+  date: string;
 }
 
-const PostComponent:React.FC<PostComponentProps> = ({text, comments, likes, creatorName, date}) => {
+const PostComponent = (props: PostComponentProps) => {
+  const [liked, setLiked] = useState(true);
   return (
-      <Card className="post" variant="outlined">
-
+    <Card className="post" variant="outlined">
+      <>
+        <ListItem sx={{ pl: 1 }}>
+          <ListItemIcon>
+            <Avatar alt={props.creatorName} src="/static/images/avatar/1.jpg" />
+          </ListItemIcon>
+          <ListItemText primary={props.creatorName} />
+        </ListItem>
+      </>
       <div className="post-content">
-        <p className="post-text">{text}</p>
+        <p className="post-text">{props.text}</p>
       </div>
-       <CardContent>
-        <div className="post-likes">Likes: {likes}</div>
-        <div className="post-creator">Creator: {creatorName}</div>
-        <div className="post-date">Date: {date}</div>
-       </CardContent>
-       {comments.length > 0 && <div className="post-comments">
-       <div className="post-comments-title">Comments</div>
-        {comments.map((comment, index) => (
-          <div className="comment" key={index}>{comment}</div>
-        ))}
-      </div>}
-      </Card>
+      <CardContent>
+        <div>
+          <Stack
+            direction="row"
+            sx={{
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <ListItemIcon
+              onClick={() => setLiked(!liked)}
+              sx={{
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              {liked ? (
+                <FavoriteBorderIcon className="post-likes"  />
+              ) : (
+                <FavoriteIcon className="post-likes" />
+              )}
+              <ListItemText sx={{ pl: 1 }} primary={`Likes: ${props.likes}`} />
+            </ListItemIcon>
+
+            <ListItemText
+              primary={props.date}
+              sx={{ textAlign: "right" }}
+            />
+          </Stack>
+        </div>
+        {/* <div className="post-date">Date: {props.date}</div> */}
+      </CardContent>
+
+      <CommentSection comments={props.comments.comments} />
+    </Card>
   );
 };
 
-
 export default PostComponent;
-export type { PostComponentProps };
