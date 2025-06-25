bb47567f10d9c14a7ca944fb95a0262c33d03838
DESKTOP-TEN4NAB\user <ran.fridman.06@gmail.com>
2025-06-25 09:46:51 +0300

    Updated changes from different branch

--- kerem-net/src/App.tsx
+++ kerem-net/src/App.tsx
@@ -1,26 +1,14 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
-import HomePage, { HomePageProps } from "./features/HomePage/HomePage";
-import UserPage from "./features/UsersPage/userPage";
-import postsData from "./Data/posts.json";
-import usersData from "./Data/users.json";
 import { useDemoRouter } from "@toolpad/core/internal";
 import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { AppProvider } from "@toolpad/core/AppProvider";
-
+import Pages from "./features/Pages/Pages";
 import { NAVIGATION } from "./features/Navigation/Navigation";
 import { lightTheme, darkTheme } from "./features/Theme/themes";
 function App() {
-  const posts: HomePageProps["initialPosts"] = postsData;
   const router = useDemoRouter("/posts");
-
-  const pages: Record<string, React.ReactElement> = {
-    "/posts": <HomePage initialPosts={posts} />,
-    "/users": <UserPage initialUsers={usersData} />,
-    "/profile": <div>Profile Page</div>,
-  };
-
   return (
     <div className="App">
       <AppProvider
@@ -30,10 +18,9 @@ function App() {
           title: "KeremNet",
           logo: <img src={logo} alt="logo" className="app-logo" />,
         }}
-        theme={{ dark: darkTheme, light: lightTheme }}
-      >
+        theme={{ dark: darkTheme, light: lightTheme }}>
         <DashboardLayout>
-          {pages[router.pathname as keyof typeof pages] ?? <div>404</div>}
+          {Pages[router.pathname] ?? <div>404</div>}
         </DashboardLayout>
       </AppProvider>
     </div>
