0ca300c417633b5606d0a9736960dd210f6af57d
DESKTOP-TEN4NAB\user <ran.fridman.06@gmail.com>
2025-06-26 09:00:33 +0300

    Added changes

--- kerem-net-nest/src/posts/posts.service.ts
+++ kerem-net-nest/src/posts/posts.service.ts
@@ -8,7 +8,7 @@ import { v4 } from "uuid";
 import { User, UsersService } from '../users/users.service';
 import postsDate from '../Data/posts.json'
 import { CreatePostDto } from './posts.controller';
-import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
 
 
 export interface PostInfo {
@@ -16,13 +16,10 @@ export interface PostInfo {
     userId: string,
     text: string;
     comments: {
-        comments:
-        {
-            commentContent: string;
-            userName: string;
-            date: string;
-        }[]
-    };
+        commentContent: string;
+        userName: string;
+        date: string;
+    }[],
     likes: string[];
     creatorName: string;
     date: string;
@@ -32,29 +29,31 @@ export class PostsService {
     constructor(private usersService: UsersService) { }
     private posts: PostInfo[] = postsDate;
     getAllPosts(): PostInfo[] {
-        console.log(this.posts);
-
         return this.posts;
     }
 
-    getPostById(id: string): PostInfo | string {
-        return this.posts.find(post => post.id === id) || 'Post was not found';
+    getPostById(id: string): PostInfo {
+        const post = this.posts.find(post => post.id === id);
+        if (!post) {
+            throw new NotFoundException(`Post with id ${id} not found`);
+        }
+        return post;
     }
-    createPost(post: CreatePostDto): PostInfo {
+    createPost({userId, text, creatorName}: CreatePostDto): PostInfo {
         const newpost: PostInfo = {
             id: (v4() as string),
-            userId: post.userId,
-            text: post.text,
-            comments: { comments: [] },
+            userId: userId,
+            text: text,
+            comments: [],
             likes: [],
-            creatorName: post.creatorName,
+            creatorName: creatorName,
             date: new Date().toISOString()
         };
         this.posts.push(newpost);
         return newpost;
     }
     toggleLike(userId: string, postId: string): any {
-        const user: User | undefined = this.usersService.getUserById(userId);
+        const user = this.usersService.getUserById(userId);
         const post = this.getPostById(postId);
         if (!user || !post || typeof post === 'string') {
             throw new NotFoundException('Resource not found');
--- kerem-net-nest/src/users/users.service.ts
+++ kerem-net-nest/src/users/users.service.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable prettier/prettier */
 import { v4 } from "uuid";
+import { NotFoundException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
-import usersDate from '../Data/users.json'
+import usersData from '../Data/users.json'
 import { CreateUserDto } from "./users.controller";
 export interface User {
     id: string,
@@ -17,54 +18,53 @@ export interface User {
 
 @Injectable()
 export class UsersService {
-    private users: User[] = usersDate as User[];
+    private users: User[] = usersData as User[];
 
     getAllUsers(): User[] {
         return this.users;
     }
 
-    getUserById(id: string): User | undefined {
-        return this.users.find(user => user.id === id);
+    getUserById(id: string): User {
+        const user = this.users.find(user => user.id === id);
+        if (!user) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+        return user
     }
 
-    createUser({biography, username}: CreateUserDto): User {
+    createUser({ biography, username }: CreateUserDto): User {
         const newUser = {
             id: (v4() as string),
             followers: [],
             following: [],
             liked: [],
-            username: username,
-            biography: biography
-        } as User;
-        
+            username,
+            biography
+        };
         this.users.push(newUser);
         return newUser;
     }
 
     toggleFollow(userId: string, followerId: string) {
-        // Example implementation to use the parameters and avoid unused variable errors
-        console.log(userId, followerId  );
-        
         const user = this.getUserById(userId);
         const follower = this.getUserById(followerId);
         if (!user || !follower) {
-            return null;
+            throw new NotFoundException('Resource not found');
         }
-        if (user.followers.includes(followerId)) {  
+        if (user.followers.includes(followerId)) {
             user.followers = user.followers.filter(id => id !== followerId);
             follower.following = follower.following.filter(id => id !== userId);
         } else {
             user.followers.push(followerId);
             follower.following.push(userId);
         }
-
         return follower;
     }
 
     updateUser(id: string, items: any) {
         const user = this.getUserById(id);
         if (!user) {
-            return null;
+            throw new NotFoundException(`User with id ${id} not found`);
         }
         Object.assign(user, items);
         return user;
